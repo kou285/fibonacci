aaa93ce9269d0438af89f9563a1b20686f0ea96c
kou285 <ph0nics789@gmail.com>
2020-01-12 01:29:21 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
-// フィボナッチ数列表示する関数
-function fib(n) { // 関数fibを定義
-  if (n === 0) { // nが0だったら
-    return 0; // 0だったら終わり
-  } else if (n === 1) { // nが1だったら
-    return 1; // 1を返して終わり
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+function fib(n) {
+  if(memo.has(n)) {
+    return memo.get(n);
   }
-  return fib(n - 1) + fib(n - 2); // 上記以外の数は、nから1を引いたものと2を引いたもの、を足す
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
 }
-
-// 40まで表示するfor文を作る
-const length = 40; // lengthに40を代入　→　これは繰り返す回数
-for (let i = 0; i <= length; i++) { // 0から40まで
-  console.log(fib(i)) // consoleで表示する　→　fib関数に40を入れる
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
 }
